43a7ce393efd2445a580e37dbe0f5120063c1f01
scinavro <rulesky_kim@snu.ac.kr>
2021-10-16 07:07:23 +0900

    feat: seperate image array from main ino file into header file

---
+++ arduino/e ink display/FeatherWingTest_DifferentImages/imageArray.h
@@ -0,0 +1,11 @@
+#ifndef imageArray_h
+#define imageArray_h
+
+extern const unsigned char epd_bitmap_headspace1[296 * (128 / 8)];
+extern const unsigned char epd_bitmap_headspace2[296 * (128 / 8)];
+extern const unsigned char epd_bitmap_headspace3[296 * (128 / 8)];
+extern const unsigned char epd_bitmap_headspace4[296 * (128 / 8)];
+extern const unsigned char epd_bitmap_headspace5[296 * (128 / 8)];
+extern const unsigned char epd_bitmap_headspace6[296 * (128 / 8)];
+
+#endif
